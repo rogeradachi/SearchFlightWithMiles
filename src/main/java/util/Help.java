9c046a78d612799f9b9eeec72b4ae4c6a98c4a17
Rogerio Adachi <w51_radachi@uolinc.com>
2014-09-24 19:40:20 -0300

    Help class

---
+++ src/main/java/util/Help.java
@@ -0,0 +1,46 @@
+package util;
+
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import bycompany.GolNationalAttendedAirports;
+import bycompany.TamNationalAttendedAirports;
+import enums.NationalAirports;
+
+public class Help {
+	public static boolean exists(WebDriver driver, String xpath) {
+		boolean exists = false;
+		try {
+			exists = driver.findElements(By.xpath(xpath)).size() > 0;
+		} catch (Exception ex) {
+			exists = false;
+		}
+		return exists;
+	}
+
+	public static ArrayList<NationalAirports> attendedByTam(ArrayList<NationalAirports> airports) {
+		ArrayList<NationalAirports> attended = new ArrayList<NationalAirports>();
+		for (int i = 0; i < airports.size(); i++) {
+			NationalAirports nationalAirports = airports.get(i);
+			if (TamNationalAttendedAirports.hasTamAirport(nationalAirports)) {
+				attended.add(nationalAirports);
+			}
+		}
+		
+		return attended;
+	}
+	
+	public static ArrayList<NationalAirports> attendedByGol(ArrayList<NationalAirports> airports) {
+		ArrayList<NationalAirports> attended = new ArrayList<NationalAirports>();
+		for (int i = 0; i < airports.size(); i++) {
+			NationalAirports nationalAirports = airports.get(i);
+			if (GolNationalAttendedAirports.attendsAirport(nationalAirports)) {
+				attended.add(nationalAirports);
+			}
+		}
+		
+		return attended;
+	}
+}
