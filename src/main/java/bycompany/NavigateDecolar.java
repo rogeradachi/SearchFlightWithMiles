44beecf7055e3eba74d27cad535c33199a63b249
rogerioadachi@gmail.com <rogerioadachi@gmail.com>
2014-10-25 20:13:37 -0200

    First Commit decolar

--- src/main/java/bycompany/NavigateDecolar.java
+++ src/main/java/bycompany/NavigateDecolar.java
@@ -1,5 +1,17 @@
 package bycompany;
 
-public class NavigateDecolar {
+import java.util.HashMap;
 
+import model.SearchFilter;
+import model.SearchToolInstance;
+import navigation.DateManager;
+import navigation.TripManager;
+
+public class NavigateDecolar extends SearchToolInstance {
+	public NavigateDecolar(HashMap<String, String> urls, SearchFilter flt, TripManager trip_m, DateManager dt_m) {
+		this.url = urls.get("decolar");
+		this.flt = flt;
+		this.trip_m = trip_m;
+		this.dt_m = dt_m;
+	}
 }
